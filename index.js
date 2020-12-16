dea7e45499df2ffc50fd78345c4bea8466f76422
zach6585 <zmines@wesleyan.edu>
2020-12-15 21:46:34 -0500

    commit tree 4d24cf7a8804adff2120350d13c97b6050782d7a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+let body = document.querySelector("body");
+body.removeChild(body.querySelector("main#main"));
+
+
+
+const newHeader = document.querySelector("h1#victory");
